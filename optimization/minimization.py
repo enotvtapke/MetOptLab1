2d8c43ca9d65f893da2d366ed1e36a6f92b37c0e
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-02-21 22:04:14 +0300

    Fixed typo

--- optimization/minimization.py
+++ optimization/minimization.py
@@ -5,10 +5,10 @@ def to_scalar(fun, init_point, direction):
     return lambda x: fun(*(init_point + x * direction))
 
 
-def min_in_direction(fun, init_point, direction, method="dihotomy"):
+def min_in_direction(fun, init_point, direction, method="dichotomy"):
     g = to_scalar(fun, init_point, direction)
     bounds = opt.find_unimodal_interval(g, 0)
-    if method == "dihotomy":
+    if method == "dichotomy":
         g_min = opt.dichotomy(g, bounds, max_iter=2)
     else:
         raise ValueError("Unknown method")
