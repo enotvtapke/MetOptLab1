bc6f3c16b14daf34627bf7b2ca56b4e97789a995
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-02-21 18:06:41 +0300

    Implemented dihotomy method and gradient descent on its basis

---
+++ optimization/__init__.py
@@ -0,0 +1,4 @@
+from optimization.scalar_minimization import *
+from optimization.minimization import *
+from optimization.shedulers import *
+from optimization.utils import *
---
+++ optimization/scalar_minimization.py
@@ -0,0 +1,34 @@
+import numpy as np
+import optimization.utils as utils
+
+
+def dihotomy(fun, bounds, delta=0.05, maxiter=500, log=False):
+    a, b = bounds
+    step = 0
+    points = np.zeros(maxiter + 1)
+    while True:
+        x1 = (a + b) / 2 - delta
+        x2 = (a + b) / 2 + delta
+        if fun(x1) < fun(x2):
+            b = x2
+        else:
+            a = x1
+        points[step] = (a + b) / 2
+        step += 1
+        if step > maxiter:
+            return points if log else (a + b) / 2
+
+
+def find_unimodal_interval(fun, a, initial_lr=0.05, max_lr=5):
+    grad = utils.grad
+    if grad(fun, a) > 0:
+        raise ValueError("Function should decrease in point a.")
+    lr = initial_lr
+    b = a
+    while grad(fun, b) <= 0:
+        b += lr
+        lr = min(lr * 2, max_lr)
+    return np.array([a, b])
+
+def _grad(fun, h=1e-5):
+    return lambda x: utils.grad(fun, x, h)
