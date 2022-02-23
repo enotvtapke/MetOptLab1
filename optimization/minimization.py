cf7bbd975c589c49badba9dcc2b45b173d70bb61
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-02-23 10:42:06 +0300

    Optimize code to work well with multivariable functions. Add some examples. Fix codestyle

--- optimization/minimization.py
+++ optimization/minimization.py
@@ -1,13 +1,15 @@
+import numpy as np
+
 import optimization as opt
 
 
 def to_scalar(fun, init_point, direction):
-    return lambda x: fun(*(init_point + x * direction))
+    return lambda x: fun(init_point + x[0] * direction)
 
 
 def min_in_direction(fun, init_point, direction, method="dichotomy"):
     g = to_scalar(fun, init_point, direction)
-    bounds = opt.find_unimodal_interval(g, 0)
+    bounds = opt.find_unimodal_interval(g, np.array([0]))
     if method == "dichotomy":
         g_min = opt.dichotomy(g, bounds, max_iter=2)
     else:
--- optimization/utils.py
+++ optimization/utils.py
@@ -1,16 +1,14 @@
-from inspect import signature
-
 import numpy as np
 
 
 def grad(fun, x, h=1e-5):
     if not callable(fun):
         raise ValueError("fun should be callable")
-    dim = len(signature(fun).parameters)
+    dim = len(x)
     g = np.zeros(dim)
     step = np.zeros(dim)
     for i in range(dim):
         step[i] = h
-        g[i] = (fun(*(x + step)) - fun(*(x - step))) / (2 * h)
+        g[i] = (fun(x + step) - fun(x - step)) / (2 * h)
         step[i] = 0
     return g
